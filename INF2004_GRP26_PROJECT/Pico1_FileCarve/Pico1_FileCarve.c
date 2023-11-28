1dd7519058519103a0fa978f33106e5017de0b80
Aung Thura Zaw <33411285+Thura98@users.noreply.github.com>
2023-11-29 00:33:55 +0800

    Update Pico1_FileCarve.c
    Comments added

--- INF2004_GRP26_PROJECT/Pico1_FileCarve/Pico1_FileCarve.c
+++ INF2004_GRP26_PROJECT/Pico1_FileCarve/Pico1_FileCarve.c
@@ -11,12 +11,8 @@
 #define UART_ID uart0
 #define BAUD_RATE 115200
 
-// We are using pins 0 and 1, but see the GPIO function select table in the
-// datasheet for information on which other pins can be used.
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
-
-BYTE bytearr[2048];
+#define UART_TX_PIN 0  // PIN 0  for UART Transmission
+#define UART_RX_PIN 1  // PIN 1  for UART Receive
 
 int read_disk_sectors(
     BYTE pdrv,   /* Physical drive number to be checked (all data on the drive will be lost) */
@@ -25,20 +21,12 @@ int read_disk_sectors(
 {
     UINT m;
     UINT n;
-    UINT j;
-    UINT k;
-    DWORD lba = 1;
+    DWORD lba = 1; // variable to store the sector number
     BYTE *pbuff = (BYTE *)buff;
     DSTATUS ds;
     DRESULT dr;
 
-    // FRESULT fr;
-    // FATFS fs;
-    // FIL fil;
-    // int ret;
-    // char filename[] = "test1001.jpg";
-    // char buf[100];
-
+    // Initialize SD Card disk
     printf(" disk_initalize(%u)", pdrv);
     ds = disk_initialize(pdrv);
     if (ds & STA_NOINIT)
@@ -51,31 +39,23 @@ int read_disk_sectors(
         printf(" - ok.\n");
     }
 
-    // // Mount drive
-    // fr = f_mount(&fs, "0:", 0);
-    // if (fr != FR_OK)
-    // {
-    //     printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
-    //     while (true);
-    // }
-
-    // // Open file for writing ()
-    // fr = f_open(&fil, filename, FA_WRITE | FA_CREATE_ALWAYS);
-    // if (fr != FR_OK)
-    // {
-    //     printf("ERROR: Could not open file for writing (%d)\r\n", fr);
-    //     while (true);
-    // } 
-
-    /* Single sector read test */
+    // Disk read operation is performed in batches of 2048 bytes, which is 4 sectors by 4 sectors
+    // 1 sector contains 512 bytes
+    // The pico will crash if more than 2048 bytes of data is processed
     printf("**** Read from disk sector ****\n");
-    lba = 16639;
+    lba = 16639; // Start reading from disk sector 16639
     printf(" disk_read(%u, 0x%X, %lu, 4)", pdrv, (UINT)pbuff, lba);
     printf("\n");
+    // To read 61 sectors, the disk read operation will be performed (61/4) times
+    // Added 2 due to receiving wrong results with just a (61/4)
     for(m = 0 ; m < (61/4) + 2 ; m ++){
+        // Performs a disk read operation
         dr = disk_read(pdrv, pbuff, lba, 4);
         for (n = 0; n < 2048; n++)
         {
+            // Starts sending bytes to the second pico when the bytes ff d8 ff e0 are detected
+            // ff d8 ff e0 is the file signature for jpg image files
+            // File carving begins here
             if(pbuff[n] == 0xe0 && pbuff[n-1] == 0xff && pbuff[n-2] == 0xd8 && pbuff[n-3] == 0xff){
                 uart_putc(UART_ID, pbuff[n - 3]);
                 uart_putc(UART_ID, pbuff[n - 2]);
@@ -85,10 +65,12 @@ int read_disk_sectors(
                 printf("%02x ", pbuff[n-2]);
                 printf("%02x ", pbuff[n-1]);
 
+                // Continuously sends bytes over to the second pico untill the bytes ff d9 are detected 
                 while(true)
                 {  
                     uart_putc(UART_ID, pbuff[n]);
                     printf("%02x ", pbuff[n]);
+                    // Stops when the carved byes ff d9 are detected
                     if(pbuff[n-1] == 0xff && pbuff[n] == 0xd9){
                         break;
                     }
@@ -100,39 +82,20 @@ int read_disk_sectors(
                     }
                 }
             }
-            // uart_putc(UART_ID, pbuff[n]);
-            // printf("%02x ", pbuff[n]);
+             // Stops when the carved byes ff d9 are detected
             if(pbuff[n-1] == 0xff && pbuff[n] == 0xd9){
                 break;
             }
         }
 
+        // Stops when the carved byes ff d9 are detected
         if(pbuff[n-1] == 0xff && pbuff[n] == 0xd9){
             break;
         }
 
-        // UINT test;
-        // ret = f_write(&fil, pbuff, 2048, &test);
-        // if (ret < 0)
-        // {
-        //     printf("ERROR: Could not write to file (%d)\r\n", ret);
-        //     f_close(&fil);
-        //     while (true);
-        // }
-
+        // Increase the sector to read from by 4
         lba = lba + 4;
     }
-
-    // // Close file
-    // fr = f_close(&fil);
-    // if (fr != FR_OK)
-    // {
-    //     printf("ERROR: Could not close file (%d)\r\n", fr);
-    //     while (true);
-    // }
-
-    // // Unmount drive
-    // f_unmount("0:");
 
     return 0;
 }
@@ -151,7 +114,7 @@ int main()
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
 
-    // Wait for user to press 'enter' to continue
+    // Wait for user to press 'A' or 'B' to continue
     printf("\r\nSD card test. Enter 'A' or 'B' to console to start.\r\n");
     while (true)
     {
@@ -162,12 +125,16 @@ int main()
         }
     }
 
+    // Program exits if write block have been tampered with
     if(FF_FS_READONLY == 0 && FF_FS_LOCK > 0){
         printf("WRITE BLOCK HAS BEEN TAMPERED WITH\n");
         printf("EXITING....\n");
         return 0;
     }
 
+    // Verifies that write block is enabled by setting the
+    // Write block done by setting the FF_FS_READONLY variable to 1 and FF_FS_LOCK variable to 0
+    // In the ffconf.h file
     if(FF_FS_READONLY == 1 && FF_FS_LOCK == 0){
         printf("FF_FS_READONLY = %d\n", FF_FS_READONLY);
         printf("FF_FS_LOCK = %d\n", FF_FS_LOCK);
@@ -184,7 +151,7 @@ int main()
     int rc;
     DWORD buff[FF_MAX_SS]; /* Working buffer (4 sector in size) */
 
-    /* Read disk sectors of the physical drive #0 */
+    /* Read disk sectors of the physical drive #0 for file varving */
     rc = read_disk_sectors(0, buff);
 
     // Loop forever doing nothing
